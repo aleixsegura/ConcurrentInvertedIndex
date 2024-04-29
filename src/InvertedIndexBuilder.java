b52e4e54d4f8d9495cdbf0453d68e0f45cb1e66d
Aleix Segura <83470660+aleixsegura@users.noreply.github.com>
2024-04-29 23:43:39 +0200

    removed: authors ID data

--- src/InvertedIndexBuilder.java
+++ src/InvertedIndexBuilder.java
@@ -2,8 +2,8 @@
 Práctica 1.
 Código fuente: InvertedIndexBuilder.java
 Grau Informàtica
-48056540H - Aleix Segura Paz.
-21161168H - Aniol Serrano Ortega.
+Aleix Segura Paz.
+Aniol Serrano Ortega.
 --------------------------------------------------------------- */
 import java.util.ArrayList;
 import java.util.Map;
